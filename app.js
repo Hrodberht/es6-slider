f9457f6eda7558c464820d284c2b7e1a074fb3dc
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-10 07:42:38 +0100

    Added query selectors for container and buttons

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 import people from "./data.js";
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
