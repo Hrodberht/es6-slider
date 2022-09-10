2f538880ff6f15d30ec6d8f9100b22d378a66a4c
Hrodberht <hrodberht.stefanescu@gmail.com>
2022-09-10 07:47:03 +0100

    Added dynamic slides

--- app.js
+++ app.js
@@ -3,3 +3,25 @@ import people from "./data.js";
 const container = document.querySelector(".slide-container");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
+
+// set slides
+container.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, job, text } = person;
+    return `<article class="slide">
+          <img
+            src="${img}"
+            class="img"
+            alt="${name}"
+          />
+          <h4>${name}</h4>
+          <p class="title">${job}</p>
+          <p class="text">
+            ${text}
+          </p>
+          <div class="quote-icon">
+            <div class="fas fa-quote-right"></div>
+          </div>
+        </article>`;
+  })
+  .join("");
